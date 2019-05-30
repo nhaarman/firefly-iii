f5032685eef5f207bb2cde4d4e33e4e8f9485a9d
Niek Haarman <haarman.niek@gmail.com>
2019-05-30 11:30:25 +0200

    Include upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+rm -rf bootstrap/cache/*
+rm -rf vendor/
+composer install --no-scripts --no-dev
+composer install --no-dev
+php artisan migrate --seed
+php artisan firefly-iii:decrypt-all
+php artisan cache:clear
+php artisan firefly-iii:upgrade-database
+php artisan passport:install
+php artisan cache:clear
